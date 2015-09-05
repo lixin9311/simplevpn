021694873b98a95820cdb45f903bb5223af4d371
lucus <lixin9311@gmail.com>
2015-09-05 11:22:52 +0800

    add pipe function.

---
+++ securesocket/pipe.go
@@ -0,0 +1,35 @@
+package securesocket
+
+import (
+	"io"
+	"log"
+)
+
+func Pipe(src io.Reader, dst io.Writer) {
+	buf := leakyBuf.Get()
+	defer leakyBuf.Put(buf)
+	defer log.Println("Pipe Broken")
+	for {
+		n, err := src.Read(buf)
+		// read may return EOF with n > 0
+		// should always process n > 0 bytes before handling error
+		if n > 0 {
+			// Note: avoid overwrite err returned by Read.
+			if _, err := dst.Write(buf[0:n]); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+		if err != nil {
+			// Always "use of closed network connection", but no easy way to
+			// identify this specific error. So just leave the error along for now.
+			// More info here: https://code.google.com/p/go/issues/detail?id=4373
+			/*
+				if bool(Debug) && err != io.EOF {
+					Debug.Println("read:", err)
+				}
+			*/
+			break
+		}
+	}
+}
